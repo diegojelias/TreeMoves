6fe2048246748424829dc2e77aa3781bac1b3e21
Genevieve Mount <gmount1@lsu.edu>
2017-06-02 12:07:27 -0500

    Adding dendropy3 compatibility

--- readTree.py
+++ readTree.py
@@ -499,14 +499,14 @@ def rf_unweighted(tree_object1,tree_object2,normalized='F'):
 	#print(tree_newick1)
 	#print(tree_newick2)
 	version = dendropy.__version__.split(".")[0]
-		if version == '4':
-			taxa = dendropy.TaxonNamespace() #set taxa same for all 
-			tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
-			tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
-		elif version == '3':
-			taxa = dendropy.TaxonSet() #set taxa same for all 
-			tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_set=taxa, rooting='force-rooted')
-			tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_set=taxa, rooting='force-rooted')
+	if version == '4':
+		taxa = dendropy.TaxonNamespace() #set taxa same for all 
+		tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
+		tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
+	elif version == '3':
+		taxa = dendropy.TaxonSet() #set taxa same for all 
+		tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_set=taxa, rooting='force-rooted')
+		tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_set=taxa, rooting='force-rooted')
 	tree1.encode_bipartitions()
 	tree2.encode_bipartitions()
 	dist=dendropy.calculate.treecompare.symmetric_difference(tree1,tree2)
@@ -524,14 +524,14 @@ def rf_weighted(tree_object1,tree_object2):
 	#print(tree_newick1)
 	#print(tree_newick2)
 	version = dendropy.__version__.split(".")[0]
-		if version == '4':
-			taxa = dendropy.TaxonNamespace() #set taxa same for all 
-			tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
-			tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
-		elif version == '3':
-			taxa = dendropy.TaxonSet() #set taxa same for all 
-			tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_set=taxa, rooting='force-rooted')
-			tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_set=taxa, rooting='force-rooted')
+	if version == '4':
+		taxa = dendropy.TaxonNamespace() #set taxa same for all 
+		tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
+		tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_namespace=taxa, rooting='force-rooted')
+	elif version == '3':
+		taxa = dendropy.TaxonSet() #set taxa same for all 
+		tree1=dendropy.Tree.get(data=tree_newick1,schema='newick',taxon_set=taxa, rooting='force-rooted')
+		tree2=dendropy.Tree.get(data=tree_newick2,schema='newick',taxon_set=taxa, rooting='force-rooted')
 	tree1.encode_bipartitions()
 	tree2.encode_bipartitions()
 	dist=dendropy.calculate.treecompare.weighted_robinson_foulds_distance(tree1,tree2)
